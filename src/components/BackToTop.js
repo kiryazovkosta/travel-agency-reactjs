f298e7f0c4e01bc11eb6ae49947c7d7fed0f0e63
kiryazovkosta <kosta.kiryazov@gmail.com>
2023-09-01 09:57:32 +0300

    Add register and login buttons

---
+++ src/components/BackToTop.js
@@ -0,0 +1,7 @@
+function BackToTop() {
+    return (
+        <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="fa fa-angle-double-up"></i></a>
+    )
+}
+
+export default BackToTop;
---
+++ src/components/Topbar.js
@@ -0,0 +1,39 @@
+function Topbar() {
+    return (
+        <div className="container-fluid bg-light pt-3 d-none d-lg-block">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 text-center text-lg-left mb-2 mb-lg-0">
+                        <div className="d-inline-flex align-items-center">
+                            <p><i className="fa fa-envelope mr-2"></i>info@example.com</p>
+                            <p className="text-body px-3">|</p>
+                            <p><i className="fa fa-phone-alt mr-2"></i>+012 345 6789</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 text-center text-lg-right">
+                        <div className="d-inline-flex align-items-center">
+                            <a className="text-primary px-3" href="#">
+                                <i className="fab fa-facebook-f"></i>
+                            </a>
+                            <a className="text-primary px-3" href="#">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                            <a className="text-primary px-3" href="#">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                            <a className="text-primary px-3" href="#">
+                                <i className="fab fa-instagram"></i>
+                            </a>
+                            <a className="text-primary pl-3" href="#">
+                                <i className="fab fa-youtube"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Topbar;
